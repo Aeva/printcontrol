e44536812c07b0721ab5bfae873dc99e38bf5598
Aeva Ntsc <aeva.ntsc@gmail.com>
2013-06-28 19:15:11 -0500

    Controller now only creates gauges when it recieves temperature reports.  Controller may now recieve temperature reports.

--- printcontrol/controller.py
+++ printcontrol/controller.py
@@ -16,7 +16,7 @@
 # along with Printcontrol.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import os
+import os, json
 from gi.repository import Gtk
 from switchprint.printer_interface import PrinterInterface
 from crosshair import Crosshair
@@ -40,17 +40,44 @@ class PrinterController(PrinterInterface):
 
         self.controls = self.builder.get_object("control_widgets_box")
         self.crosshair = Crosshair(self)
-        self.gauges = [
-            BedGauge(self),
-            ExtruderGauge(self, 0),
-            ExtruderGauge(self, 1)
-            ]
+        self.gauges = {
+            "b" : None,
+            "t" : [],
+            }
         self.update_controls()
         self.disable()
         self.set_printer_name("Unknown Printer")
 
         PrinterInterface.__init__(self, printer_uuid)
+
+    def add_gauges(self, temps):
+        """Adds any necessary gaguges from a given temperature
+        report."""
         
+        dirty = False
+        if temps.has_key("bed") and temps["bed"][0] > 0:
+            if self.gauges["b"] is None:
+                dirty = True
+                self.gauges["b"] = BedGauge(self)
+
+        if temps.has_key("tools"):
+            while len(temps["tools"]) > len(self.gauges["t"]):
+                tool_num = len(self.gauges["t"])
+                self.gauges["t"].append(ExtruderGauge(self, tool_num))
+                dirty = True
+        if dirty:
+            self.update_controls()
+
+    def get_gauges(self):
+        """Return a list of all gauge widgets without context."""
+        
+        gauges = []
+        if self.gauges["b"] is not None:
+            gauges.append(self.gauges["b"])
+        if self.gauges["t"]:
+            gauges += self.gauges["t"]
+        return gauges
+
     def update_controls(self):
         """Clears out the controls packing box, and then populates it
         with a crosshair widget and some number of temperature
@@ -59,10 +86,11 @@ class PrinterController(PrinterInterface):
         for child in self.controls.get_children():
             self.controls.remove(child)
         self.controls.add(self.crosshair.widget)
-        for gauge in self.gauges:
+    
+        for gauge in self.get_gauges():
             self.controls.add(gauge.widget)
             gauge.disable()
-
+            
     def refocus(self):
         """Called to reset the ui state.  Currently, this is only
         really used by the crosshair when the notebook page changes.
@@ -72,7 +100,7 @@ class PrinterController(PrinterInterface):
     def disable(self):
         """Disable the controls for this printer."""
         self.crosshair.disable()
-        for gauge in self.gauges:
+        for gauge in self.get_gauges():
             gauge.disable()
 
         motors_off = self.builder.get_object("motors_off_button")
@@ -81,7 +109,7 @@ class PrinterController(PrinterInterface):
     def enable(self):
         """Enable this printer's controls."""
         self.crosshair.enable()
-        for gauge in self.gauges:
+        for gauge in self.get_gauges():
             gauge.enable()
         
         motors_off = self.builder.get_object("motors_off_button")
@@ -93,6 +121,16 @@ class PrinterController(PrinterInterface):
             self.enable()
         elif state == "offline":
             self.disable()
+
+    def on_report(self, blob):
+        packet = json.loads(blob)
+        if packet.has_key("thermistors"):
+            temps = packet['thermistors']
+            self.add_gauges(temps)
+            self.gauges["b"].set_label(temps["bed"][0])
+            for gauge, temp in zip(self.gauges["t"], temps["tools"]):
+                gauge.set_label(temp[0])
+            
         
     def set_printer_name(self, name):
         """Sets the displayed name for this printer."""
@@ -101,12 +139,12 @@ class PrinterController(PrinterInterface):
 
     def on_focus_in(self, widget, event_info):
         self.crosshair.on_focus_in(widget, event_info)
-        for gauge in self.gauges:
+        for gauge in self.get_gauges():
             gauge.on_focus_in(widget, event_info)
 
     def on_focus_out(self, widget, event_info):
         self.crosshair.on_focus_out(widget, event_info)
-        for gauge in self.gauges:
+        for gauge in self.get_gauges():
             gauge.on_focus_out(widget, event_info)
 
     def on_motors_off(self, *args):
